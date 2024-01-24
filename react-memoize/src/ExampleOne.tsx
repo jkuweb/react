19e608cf2a0612201fe7d2801b645f2d2ae095c4
Joseba <josebaquerejetaweb@gmail.com>
2024-01-24 10:18:12 +0100

    change example-one, react-memo with object, not with primitive value

--- react-memoize/src/ExampleOne.tsx
+++ react-memoize/src/ExampleOne.tsx
@@ -1,25 +1,49 @@
 import React from "react";
 
 export const ExampleOne = () => {
-  const [name, setName] = React.useState('joseba')
-  const [count, setCount] = React.useState(0)
+  const [user, setUser] = React.useState({
+    name: "joseba",
+    lastName: "querejeta",
+  });
+  const [count, setCount] = React.useState(0);
+  return (
+    <div style={{ marginTop:'4rem', display:'grid' }}>
+      <h2>Example 1 : React.memo</h2>
+      <div>
+        <button onClick={() => setCount(count + 1)}>counter {count}</button>
+      </div>
+      <input
+        type="text"
+        value={user.name}
+        onChange={(e) =>
+          setUser({
+            ...user,
+            name: e.target.value,
+          })
+        }
+      />
+      <input
+        type="text"
+        value={user.lastName}
+        onChange={(e) =>
+          setUser({
+            ...user,
+            lastName: e.target.value,
+          })
+        }
+      />
+        <MyChildComponent name={user.name} />
+    </div>
+  );
+};
+// Sí no me cambias la entrada te devuelvo la última entrada que tengo guardada
+const MyChildComponent = React.memo(({ name }) => {
+  console.log("MyChildComponent -> me estoy renderizando");
   return (
     <>
-      <h2>Example 1 : primitive value (inmutable)</h2>
-      <div>
-      <button onClick={()=> setCount(count + 1)}>counter {count}</button>
-      </div>
-      <MyChildComponent name={name}/>
-      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+      <p>
+        {name} 
+      </p>
     </>
-  )
-}
-
-const MyChildComponent = React.memo(({name}) => {
-  console.log('me estoy renderizando')
-    return ( 
-    <>
-    <p>{name}</p>
-    </>
-    )
-})
+  );
+});
