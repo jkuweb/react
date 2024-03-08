067ae66bd5fe17b5d4117f997530be755e4cd5da
Joseba <josebaquerejetaweb@gmail.com>
2024-03-08 18:28:39 +0100

    show mock with movies && mapped movie entity && create Movie component

--- react-movie-search/src/App.tsx
+++ react-movie-search/src/App.tsx
@@ -1,9 +1,33 @@
+import React from "react";
+import moviesResult from "./withResults.json";
+import { Movies } from "./components/Movies";
 import "./App.css";
 
-export const App = () => {
+export const useMovies = () => {
+  const movies = moviesResult.Search;
+  const mappedMovies = movies.map((movie) => ({
+    id: movie.imdbID,
+    title: movie.Title,
+    year: movie.Year,
+    image: movie.Poster,
+  }));
+  return { movies: mappedMovies };
+};
+
+export const App: React.FC = () => {
+  const { movies } = useMovies();
   return (
     <>
-      <h1>Hello React</h1>
+      <header className="header">
+        <h1>Movie Search</h1>
+        <div className="header__search-form">
+          <input type="text" placeholder="avangers, start wars..." />
+          <button>Search</button>
+        </div>
+      </header>
+      <main className="main">
+        <Movies movies={movies} />
+      </main>
     </>
   );
 };
---
+++ react-movie-search/src/components/Movies.tsx
@@ -0,0 +1,28 @@
+import error from "../withoutResults.json";
+
+const ListOfMovies = ({ movies }) => {
+  return (
+    <ul className="movie-result-list">
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <h3>{movie.title}</h3>
+          <img src={movie.image} alt={`poster of ${movie.title} movie`} />
+          <p>Year: {movie.year}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ErrorMoviesResult = ({ error }) => {
+  return <p>{error}</p>;
+};
+
+export const Movies = ({ movies }) => {
+  const hasMovies = movies?.length > 0;
+  return hasMovies ? (
+    <ListOfMovies movies={movies} />
+  ) : (
+    <ErrorMoviesResult error={error} />
+  );
+};
